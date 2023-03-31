6d67d891bd72f5ef3dd212733ee5c81fb2598719
Evan <evchan29@gmail.com>
2023-03-31 16:32:44 -0400

    install

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+// module.exports = {
+//   basePath: '/my-nextjs-app',
+//   assetPrefix: '/my-nextjs-app',
+// }
